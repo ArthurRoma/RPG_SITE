f02b1dc89c25ab20c26b3ca8083d5124f05328ab
Arthur <lopestui88@gmail.com>
2024-05-27 23:59:46 -0300

    Primeiro commit

--- style.js
+++ style.js
@@ -4,13 +4,13 @@
     })
 function clique(){
     if (select.value == "Danos"){
-        return window.location.href = "/templates/danos.html";
+        return window.location.href = "danos.html";
     } else if (select.value == "Armas"){
-        return window.location.href = "/templates/armas.html";
+        return window.location.href = "armas.html";
     } else if (select.value == "Cidades"){
-        return window.location.href = "/templates/cidade.html";
+        return window.location.href = "cidade.html";
     } else if(select.value == "geral"){
-        return window.location.href = "/templates/regras.html";
+        return window.location.href = "regras.html";
     } 
 }
 
